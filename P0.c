58e7df52e588a65ab4386de61390138993b02638
gustavo-alex <43149821+gustavo-alex@users.noreply.github.com>
2018-12-04 10:06:19 -0200

    Create P0.c

---
+++ P0.c
@@ -0,0 +1,18 @@
+/ * P0.c * /
+/ * Gustavo Alexande P. dos S. Sousa * /
+/ * 11711EEL005 * /
+# include  < stdio.h >
+int  main ( int argc, char ** argv) {
+
+int i;
+if (argc> 1 ) {
+if (argc> 2 )
+para (i = 0 ; i <argv [ 2 ] [ 0 ] - ' 0 ' ; i ++)
+printf ( " Valeu, % s \ n " , argv [ 1 ]);
+outro
+printf ( " Saudacoes, % s \ n " , argv [ 1 ]);
+}
+outro
+printf ( " Ola! \ n " );
+return  0 ;
+}

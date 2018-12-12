3de3c1c01cc8b77e73f9807bc76c90d3226c8e3c
gustavo-alex <43149821+gustavo-alex@users.noreply.github.com>
2018-12-12 20:07:25 -0200

    Update P3.c

--- P3.c
+++ P3.c
@@ -1,111 +1,22 @@
-/ * P2.c * /
-/ * Gustavo Alexander P. dos S. Sousa * /
-/ * 11711EEL005 * /
+//P3.c
+//Gustavo Alexander P. dos S. Sousa
+//11711EEL005
 #include <stdio.h>
-void conv2bin(int i)
+void integrarvalor(char *str,int *s)
 {
-    unsigned int u_i=0;
-    int j=0, b[32]={0};
-    u_i = (unsigned int)i;
-    for(j=31;j>=0;j--) 
-	{
-        b[j]=u_i&0x1;
-        u_i=u_i>>1;
-    }
-    for (j=0;j<=31;j++)
-        printf("%d", b[j]);
-}
-int main()
-{
-	int num=0, num2=0, menu, conv=0, bin=0, bin2=0, conv_bin=0;
-	do
-	{
-		printf("\n1. NOT \n2. AND \n3. OR \n4. XOR \n5. Right Shitf \n6. Left Shitf \n7. Sair do Programa \n:");
-		scanf("%i", &menu);
-		getchar();
-		switch(menu)
+		if(*str)
 		{
-			case 1:
-			    printf("\n: "); 
-			    scanf("%i", &num); 
-			    getchar();
-			    conv=~num;
-				printf("\nNOT %i (", num); 
-				conv2bin(num); 
-				printf(") : %i (", conv);
-				conv2bin(conv);
-				printf(")\n"); //
-			    break;
-			case 2:
-				printf("\n: "); 
-			    scanf("%i %i", &num, &num2); 
-			    getchar();
-			    conv=num&num2;
-			    printf("\n %i (", num); 
-				conv2bin(num); 
-				printf(") AND %i (", num2);
-				conv2bin(num2);
-				printf(") : %i (", conv);
-				conv2bin(conv);
-				printf(")\n");
-				break;
-			case 3:
-				printf("\n: "); 
-			    scanf("%i %i", &num, &num2); 
-			    getchar();
-			    conv=num|num2;
-			    printf("\n %i (", num); 
-				conv2bin(num); 
-				printf(") OR %i (", num2);
-				conv2bin(num2);
-				printf(") : %i (", conv);
-				conv2bin(conv);
-				printf(")\n");
-				break;
-			case 4:
-				printf("\n: "); 
-			    scanf("%i %i", &num, &num2); 
-			    getchar();
-			    conv=num^num2;
-			    printf("\n %i (", num); 
-				conv2bin(num); 
-				printf(") XOR %i (", num2);
-				conv2bin(num2);
-				printf(") : %i (", conv);
-				conv2bin(conv);
-				printf(")\n");
-				break;
-			case 5:
-				printf("\n: "); 
-			    scanf("%i %i", &num, &num2); 
-			    getchar();
-			    conv=num>>num2;
-			    printf("\n %i (", num); 
-				conv2bin(num); 
-				printf(") >> %i (", num2);
-				conv2bin(num2);
-				printf(") : %i (", conv);
-				conv2bin(conv);
-				printf(")\n");
-			case 6:
-				printf("\n: "); 
-			    scanf("%i %i", &num, &num2); 
-			    getchar();
-			    conv=num<<num2;
-			    printf("\n %i (", num); 
-				conv2bin(num); 
-				printf(") << %i (", num2);
-				conv2bin(num2);
-				printf(") : %i (", conv);
-				conv2bin(conv);
-				printf(")\n");
-			default:
-				if (menu!=7)
-				{
-				printf("\nOpcao invalida");
-				break;
-		    	}
-		}
-	}while(menu!=7);
-	return 0;
-}
+ 			if(*str>='0'&&*str<='9')
+ 				*s=*s*10+((*str-'0'));
+ 	    integrarvalor((str+1), s);
+       }
+ }
+ int main()
+ {
+ 	char str[256]="";
+ 	int s;
+ 	scanf("%s",str);
+ 	integrarvalor(&str[0], &s);
+ 	printf("%d",s);
+ 	return 0;
+ }

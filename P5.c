9e80da63b6bbb22f1d195160b6a86ee36352fc22
gustavo-alex <43149821+gustavo-alex@users.noreply.github.com>
2018-12-12 16:15:41 -0200

    Update P5.c

--- P5.c
+++ P5.c
@@ -1,82 +1,125 @@
-//P5.c
-//Gustavo Alexander P. dos S. Sousa 
-// 11711EEL005 
+// P5.c 
+// Gustavo Alexander P. dos S. Sousa
+// 11711EEL005
 
-# include  < stdio.h >
-# define  N  50
+#include <stdio.h>
 
 typedef
-   sem assinatura  longa  longa  int
+
+	unsigned long long int
+
 Bytes8;
 
 typedef
-   struct LCG {
-      Bytes8 a, c, m, rand_max, atual; 
-   }
+
+	struct LCG { Bytes8 a, c, m, rand_max, atual; }
+
 LCG;
 
-vazio  semente (LCG * r, semente Bytes8) {
-   // constantes do POSIX [de] rand48, glibc [de] rand48 [_r]
-   // ULL transforma uma constante 'int' em 'sem assinatura long long int'
-   r-> a = 0x5DEECE66DULL ;
-   r-> c = 11ULL ;
-   r-> m = ( 1ULL << 48 );
-   r-> rand_max = r-> m - 1 ;
-   r-> atual = semente;
+
+
+void semente(LCG * r, Bytes8 seed) {
+
+	r->a = 0x5DEECE66DULL;
+
+	r->c = 11ULL;
+
+	r->m = (1ULL << 48);
+
+	r->rand_max = r->m - 1;
+
+	r->atual = seed;
+
 }
 
-Bytes8 lcg_rand (LCG * r) {
-   r-> atual = (r-> a * r-> atual + r-> c )% r-> m ;
-   retorno r-> atual ;
+Bytes8 lcg_rand(LCG * r) {
+
+	r->atual = (r->a * r->atual + r->c) % r->m;
+
+	return r->atual;
+
 }
 
-double  lcg_rand_01 (LCG * r) {
-   return (( double ) lcg_rand (r)) / (r-> rand_max );
+double lcg_rand_01(LCG * r) {
+
+	return ((double) lcg_rand(r))/(r->rand_max);
+
 }
 
-void  gera_numeros_printa ( flutuante *, int , flutuante , flutuante , LCG *);
+void gera_numeros(float * vetor, int tam, float min, float max, LCG * r) {
 
-float  somatorio ( float *, float *);
-float  produtio ( float *, float *);
+	int i;
 
-int  main () {
-   LCG aleatório ;
-   semente (& aleatória , 123456 );
-   
-   vetor flutuante [N]; // porque não pode deixar * vetor?
-   char opcao;
-   printf ( " \ n \ n \ t " );
-   gera_numeros_printa (& vetor [ 0 ], N, 0.5 , 1.5 , & aleatório );
-   printf ( " \ n \ n \ t 1 - Somatorio \ n \ t 2 - Produtorio \ n \ n \ t " );
-   scanf ( " % c " e opcao);
-   switch (opcao) {
-     case  ' 1 ' :
-     printf ( " \ n \ t \" % c \ " resulta em \" % g \ "\ n \ n " , opcao, somatorio (& vetor [ 0 ] e vetor [ 0 ] + N- 1 ));
-     pausa ;
-     
-     caso  ' 2 ' :
-     printf ( " \ n \ t \" % c \ " resulta em \" % g \ "\ n \ n " , opcao, produtor (& vetor [ 0 ] e vetor [ 0 ] + N- 1 ));
-     pausa ;
-     
-     padrão :
-     	printf ( " \ n \ n \ t Opcao invalida \ n \ n " );
+	for(i = 0; i < tam; i++)
+
+		vetor[i] = (max-min)*lcg_rand_01(r) + min;
+
+}
+
+float soma(float *inicio_vetor, float *fim_vetor){
+
+	return (inicio_vetor == fim_vetor+1)? 0 : *inicio_vetor + soma(inicio_vetor+1, fim_vetor);
+
+}
+
+float produtorio(float *inicio_vetor, float *fim_vetor){
+
+	if(inicio_vetor == fim_vetor+1)
+
+		return 1;
+
+	else 	
+
+		return *inicio_vetor * produtorio(inicio_vetor+1, fim_vetor);
+
+}
+
+int main()
+
+{
+
+	LCG random;
+
+	semente(&random, 123456);
+
+	int opc, N = 50;
+
+	float vetor[50], somatorio, produto;
+
+	gera_numeros(&vetor[0], N, 0.5, 1.5, &random);
+
+	printf("1- Somatorio \n2- Produtorio\n\n");
+
+	scanf("%d", &opc); 
+
+	getchar();
+
+	switch(opc)
+
+	{
+
+    	case 1:
+
+    	somatorio = soma(&vetor[0], &vetor[0] + N-1);
+
+    	printf("%f", somatorio);
+
+    	break;
+
+    	case 2:
+
+    	produto = produtorio(&vetor[0], &vetor[0] + N-1);
+
+    	printf("%f", produto);
+
+   		break;
+
+    	default:
+
+    	printf("\n\n\tOpcao invalida\n\n");
+
    }
-   return  0 ;
-}
 
-void  gera_numeros_printa ( float * vetor, int tam, float min, flutuante max, LCG * r) {
-   int i;
-   para (i = 0 ; i <tam; i ++) {
-      vetor [i] = (max-min) * lcg_rand_01 (r) + min;
-      printf ( " % f   " , vetor [i]);
-      if ((i + 1 )% 10 == 0 ) printf ( " \ n \ t " );
-   }
-}
+	return 0;
 
-float  somatorio ( float * inicio_vetor, float * fim_vetor) {
-   return (inicio_vetor == fim_vetor + 1 )? 0 : * inicio_vetor + somatorio (inicio_vetor + 1 , fim_vetor);
-}
-
-float  produtor ( float * inicio_vetor, float * fim_vetor) {
-   return (inicio_vetor == fim_vetor + 1 )? 1 : * inicio_vetor * produtor (inicio_vetor + 1 , fim_vetor);
 }

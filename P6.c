4c3ccc0c06d865a0ce911c38e342c8f9afa8e9e0
gustavo-alex <43149821+gustavo-alex@users.noreply.github.com>
2018-12-12 10:03:13 -0200

    Update P6.c

--- P6.c
+++ P6.c
@@ -1,28 +1,27 @@
 / * P6.c * /
 / * Gustavo Alexander P. dos S. Sousa * /
 / * 11711EEL005* /
-# include  < stdio.h >
-# include  < stdlib.h >
-# include  < math.h >
-# define  pi  3.14159265359
-# define  r  1
-
-typedef  struct {
-  float x, y;
-} Ponto;
-
-int  main () {
-   int N, i; // empilhar memória
-   Ponto * xy;
-   scanf ( " % i " , & N); getchar ();
-   xy = (Ponto *) malloc (N * tamanho de (Ponto)); // memória heap
-   printf ( " % i volta \"  " , N);
-   para (i = 0 ; i <N; i ++) {
-     xy-> x = r * cos (i * 2 * pi / (N));
-     xy-> y = r * sin (i * 2 * pi / N);
-     printf ( " ( % .3f , % .3f ) " , xy-> x , xy-> y );
-   }
-   printf ( " \"  " );
-   livre (xy);
-   return  0 ;
+#include <stdio.h>
+int sm(int len, int *v)
+{
+    if(len>0)
+        return *v+sm(len-1, v+1);
+    else
+        return 0.0;
+}
+float med(int x, int y)
+{
+    return (float)x/(float)y;
+}
+int main ()
+{
+	int vet[]={0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+	int tam=0, i=0;
+	printf("Quantidade a ser somada:\n");
+	scanf("%d", &tam);
+	printf("\nValores: \n");
+	for (i=0; i<tam; i++) 
+		scanf("%d", &vet[i]);
+	printf("\nmedia = %f", med(sm(tam, vet), tam));
+	return 0;
 }
